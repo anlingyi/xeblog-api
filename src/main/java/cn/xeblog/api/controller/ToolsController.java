10a4f1c65974813e52649919382f7741c5e065d0
anlingyi <1090172196@qq.com>
2021-03-07 22:08:55 +0800

    新增网页渲染接口

--- src/main/java/cn/xeblog/api/controller/ToolsController.java
+++ src/main/java/cn/xeblog/api/controller/ToolsController.java
@@ -6,8 +6,14 @@ import cn.xeblog.api.util.CheckUtils;
 import cn.xeblog.api.util.ImageSynthesisUtils;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
+import org.apache.commons.lang3.StringUtils;
+import org.apache.http.HttpStatus;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
@@ -28,12 +34,15 @@ public class ToolsController {
 
     private final static Logger LOGGER = LoggerFactory.getLogger(ToolsController.class);
 
+    @Value("${webdriver.chrome.driver}")
+    private String chromeDrive;
+
     /**
      * 图像合成
      *
-     * @param file 待合成图片
+     * @param file       待合成图片
      * @param systemType 系统类型 1.安卓 2.iPhone
-     * @param response HttpServletResponse
+     * @param response   HttpServletResponse
      */
     @ApiOperation(value = "图像合成")
     @PostMapping("/imageSynthesis")
@@ -60,4 +69,28 @@ public class ToolsController {
             LOGGER.error("图片合成失败！", e);
         }
     }
+
+    @ApiOperation(value = "网页渲染")
+    @GetMapping("/render")
+    public String render(String url, HttpServletResponse response) {
+        String html = null;
+        try {
+            System.setProperty("webdriver.chrome.driver", chromeDrive);
+            ChromeOptions options = new ChromeOptions();
+            options.setHeadless(true);
+            WebDriver driver = new ChromeDriver(options);
+            driver.get(url);
+            html = driver.getPageSource();
+            driver.quit();
+        } catch (Exception e) {
+            LOGGER.error("url: {}, 渲染异常", url, e);
+        }
+
+        if (StringUtils.isBlank(html)) {
+            response.setStatus(HttpStatus.SC_BAD_GATEWAY);
+        }
+
+        return html;
+    }
+
 }
